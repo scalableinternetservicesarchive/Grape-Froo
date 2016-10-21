9c4b6ff48fbfb5673830e9ee3a2878c24295c6a2
Gautam Gupta <blerb795@gmail.com>
2016-10-21 02:52:29 -0700

    validation on template form fields

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,21 @@
+class UrlValidator < ActiveModel::Validator
+  # Potentially slow!!
+  def validate(record)
+    url = URI.parse(record.url)
+    if url.host.blank? || url.port.blank?
+      record.errors[:base] << "Invalid image URL"
+      return
+    end
+    request = Net::HTTP.new(url.host, url.port)
+    request.use_ssl = (url.scheme == "https")
+    response = request.head(url.path.presence || "/")
+    if !(response.code == "200" && response['Content-Type'].starts_with?("image"))
+      record.errors[:base] << "Invalid image URL"
+    end
+  end
+end
+
 class Template < ApplicationRecord
-  validates :name, presence: true, length: { in: 4..255 }
+  validates :name, uniqueness: true, presence: true, length: { in: 4..255 }
+  validates_with UrlValidator, fields: [:url]
 end
