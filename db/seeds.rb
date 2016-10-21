4cdea8757a487dcde01394eba0211dbbff66562e
Gautam Gupta <blerb795@gmail.com>
2016-10-21 00:47:12 -0700

    add some seed templates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Template.create("name"=>"confession_bear", "url"=>"http://i.imgur.com/uGNb9El.jpg")
+Template.create("name"=>"how_about_no", "url"=>"http://i.imgur.com/VLlma7X.jpg")
+Template.create("name"=>"unpopular_opinion", "url"=>"http://i.imgur.com/F1SkTlb.jpg")
+Template.create("name"=>"facepalm_picard", "url"=>"http://i.imgur.com/ST9hCiw.jpg")
+Template.create("name"=>"scumbag_steve", "url"=>"http://i.imgur.com/b9IWEug.jpg")
