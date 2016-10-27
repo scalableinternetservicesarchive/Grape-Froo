b68b0ffbaa2fe87dd55a56b0247bcba953440c4c
Gautam Gupta <blerb795@gmail.com>
2016-10-27 02:36:52 -0700

    add Users controller for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, :only => [:show]
   resources :templates
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
