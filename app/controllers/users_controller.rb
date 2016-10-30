b5b09fd2e176c0d432e6d32159c06c6326d81ecf
Alex Crosthwaite <abcrosthwaite@ucla.edu>
2016-10-29 22:20:12 -0700

    add tabs on user page for profile, memes, and templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def memes
+  	@user = User.find(params[:id])
+  end
+
+  def templates
+  	@user = User.find(params[:id])
+  end
 end
