eb4f7d2b31b5f3eb534064ce4b9fe2b65784b84d
Alex Crosthwaite <abcrosthwaite@ucla.edu>
2016-10-29 23:47:28 -0700

    adress pull request comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :memes, :templates]
+
+
   def show
-    @user = User.find(params[:id])
   end
 
   def memes
-  	@user = User.find(params[:id])
   end
 
   def templates
-  	@user = User.find(params[:id])
   end
+
+  private
+
+  	def set_user
+  		@user = User.find(params[:id])
+  	end
 end
