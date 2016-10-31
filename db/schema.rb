ccf13e25a12643457ebc5290c55e29f77b9f9bc9
Gautam Gupta <blerb795@gmail.com>
2016-10-30 22:47:52 -0700

    remove middle_caption and all associated references

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028012517) do
+ActiveRecord::Schema.define(version: 20161031053024) do
 
   create_table "memes", force: :cascade do |t|
     t.string   "url"
     t.string   "top_caption"
-    t.string   "middle_caption"
     t.string   "bottom_caption"
     t.integer  "template_id"
     t.integer  "user_id"
@@ -52,6 +51,17 @@ ActiveRecord::Schema.define(version: 20161028012517) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "meme_id"
+    t.boolean  "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meme_id"], name: "index_votes_on_meme_id"
+    t.index ["user_id", "meme_id"], name: "index_votes_on_user_id_and_meme_id", unique: true
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
