47a8ac72075a8838015446046d3f64a79c0bbdfa
Gautam Gupta <blerb795@gmail.com>
2016-10-31 02:11:08 -0700

    Update template model to use image instead of :url Add config for S3 dev and prod Push images to S3, uploaded or from URL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028012517) do
+ActiveRecord::Schema.define(version: 20161031080135) do
 
   create_table "memes", force: :cascade do |t|
     t.string   "url"
@@ -27,10 +27,13 @@ ActiveRecord::Schema.define(version: 20161028012517) do
 
   create_table "templates", force: :cascade do |t|
     t.string   "name"
-    t.text     "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["name"], name: "index_templates_on_name", unique: true
     t.index ["user_id"], name: "index_templates_on_user_id"
   end
--- test/controllers/templates_controller_test.rb
+++ test/controllers/templates_controller_test.rb
@@ -17,7 +17,7 @@ class TemplatesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create template" do
     assert_difference('Template.count') do
-      post templates_url, params: { template: { name: @template.name, url: @template.url } }
+      post templates_url, params: { template: { name: @template.name, image: @template.image } }
     end
 
     assert_redirected_to template_url(Template.last)
@@ -34,7 +34,7 @@ class TemplatesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update template" do
-    patch template_url(@template), params: { template: { name: @template.name, url: @template.url } }
+    patch template_url(@template), params: { template: { name: @template.name, image: @template.image } }
     assert_redirected_to template_url(@template)
   end
 
