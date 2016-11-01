14c3117be6e7cf1ecf85773bdaa521d3918829fa
Gautam Gupta <blerb795@gmail.com>
2016-10-31 22:59:52 -0700

    update upload path for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
       :region => 'us-west-2'
     },
     :fog_directory => 'scalableinternetservices',
-    :path => 'Grape-Froo/'
+    :path => 'Grape-Froo/:class/:filename'
   }
 end
